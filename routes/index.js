bd184881c518943e8f39ea112280d07a3ee352b1
agusev <agmcmxc@gmail.com>
2023-01-04 10:21:38 -0800

    feat: add game history page and routes  (stub)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+// @route    GET /
+// @desc     Get home page
+// @access   Private
 router.get('/', function (req, res, next) {
 	const { username, games } = req.session;
 
 	res.render('private/index', { username });
 });
+
+// @route    GET /history
+// @desc     Get history page
+// @access   Private
+router.get('/history', async function (req, res, next) {
+	res.render('private/history');
+});
+
+const handleError = (res, redirectUri) => (error) => {
+	console.log({ error });
+	res.redirect(redirectUri);
+};
 
 module.exports = router;
