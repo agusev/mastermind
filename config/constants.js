df9b865a0849445348ca613e0d302e8c71376883
agusev <agmcmxc@gmail.com>
2023-01-07 10:58:38 -0800

    feat: add arrays for game styles

--- config/constants.js
+++ config/constants.js
@@ -11,10 +11,29 @@ const TOTAL_GUESSES_REMAINED_MEDIUM = 7;
 const CODE_LEN_MEDIUM = 4;
 
 // configuration for a hard game
-const TOTAL_ATTEMPTS_HARD = 12;
+const TOTAL_ATTEMPTS_HARD = 5;
 const TOTAL_HINTS_HARD = 0;
-const TOTAL_GUESSES_REMAINED_HARD = 12;
+const TOTAL_GUESSES_REMAINED_HARD = 5;
 const CODE_LEN_HARD = 4;
+
+const TIME = 180;
+
+const ARRAY_OF_DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7'];
+const ARRAY_OF_EMOJI_SMILES = ['🙂', '😆', '😱', '🤠', '🧐', '🤪', '😤', '😌'];
+const ARRAY_OF_EMOJI_ANIMALS = ['🐶', '🐱', '🐭', '🐰', '🐼', '🐯', '🐸', '🦊'];
+const ARRAY_OF_EMOJI_FRUITS = ['🍉', '🍒', '🍇', '🥝', '🍊', '🍓', '🍍', '🍏'];
+const ARRAY_OF_EMOJI_PLANTS = ['🌵', '🌳', '🌴', '🌱', '🌲', '🍁', '🪴', '🌿'];
+const ARRAY_OF_EMOJI_COUNTRIES = [
+	'🇺🇸',
+	'🇨🇦',
+	'🇲🇽',
+	'🇫🇷',
+	'🇩🇪',
+	'🇦🇺',
+	'🇬🇧',
+	'🇧🇷',
+];
+const ARRAY_OF_NOTES = ['do', 're', 'mi', 'fa', 'so', 'la', 'ti', 'ut'];
 
 module.exports = {
 	TOTAL_ATTEMPTS_EASY,
@@ -29,4 +48,12 @@ module.exports = {
 	TOTAL_HINTS_HARD,
 	TOTAL_GUESSES_REMAINED_HARD,
 	CODE_LEN_HARD,
+	TIME,
+	ARRAY_OF_DIGITS,
+	ARRAY_OF_EMOJI_SMILES,
+	ARRAY_OF_EMOJI_ANIMALS,
+	ARRAY_OF_EMOJI_FRUITS,
+	ARRAY_OF_EMOJI_PLANTS,
+	ARRAY_OF_EMOJI_COUNTRIES,
+	ARRAY_OF_NOTES,
 };
